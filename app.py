7ae07c514578d3591ad3c49d3fe50915cf6b2cc2
1v1expert <1v1expert@gmail.com>
2019-12-09 03:23:20 +0300

    added regex for valid and a small features

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from telebot import TeleBot
 
 from config import TELEGRAM_TOKEN, CHAT_ID
 
+import csv
+
+from datetime import datetime
+
+import uuid
+
+import re
+
 # proxy settings
 from telebot import apihelper
 
@@ -11,10 +19,14 @@ bot = TeleBot(TELEGRAM_TOKEN)
 
 user_dict = dict()
 
+# for check email validity
+regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+
 
 class Request:
     def __init__(self, chat_id):
         self.chat_id = chat_id
+        self.uuid = str(uuid.uuid4())
         self.user_id = None
         self.datetime = None
         self.username = None
@@ -24,7 +36,11 @@ class Request:
         pass
     
     def save(self):
-        pass
+        self.datetime = datetime.now()
+
+        with open('data.csv', "a", newline='') as csv_file:
+            writer = csv.writer(csv_file, delimiter=';')
+            writer.writerow([self.uuid, self.user_id, self.datetime.strftime('%Y-%m-%d %H:%M'), self.username, self.email])
     
     def is_subscriber(self) -> bool:
         if bot.get_chat_member(CHAT_ID, self.user_id).status in ['creator', 'administrator', 'member']:
@@ -54,7 +70,7 @@ def init_chat(message):
 def mail_processing(message):
         chat_id = message.chat.id
         email = message.text
-        if '@' not in email:
+        if not re.search(regex, email):
             msg = bot.reply_to(message, 'That does not look like an email. Please input full email you used to sign up '
                                         'at the website.')
             bot.register_next_step_handler(msg, mail_processing)
@@ -63,16 +79,18 @@ def mail_processing(message):
             user_dict[chat_id] = request
             request.email = email
             request.user_id = message.from_user.id
+            request.username = message.from_user.username
             if not request.is_subscriber():
-                msg = bot.reply_to(message, 'You need to be subscribed to our official Channel. '
+                msg = bot.reply_to(message, 'You need to be subscribed to our official [Channel](http://google.com) '
                                             'Please click the link and join the Channel to be able to participate into '
                                             'our giveaway. ')
                 bot.register_next_step_handler(msg, mail_processing)
-            
+
+            bot.reply_to(message, 'Thnx, bro')
             request.save()
 
 
-bot.load_next_step_handlers()
+# bot.load_next_step_handlers()
 bot.polling()
 
 # https://t.me/proxy?server=ru.tgproxy.today&port=8080&secret=ddfb175d6d7f820cdc73ab11edbdcdbd74
